d718652a6dc2236fbbeae637baa263558d8a946e
Gustavo Mendes <gustavormendess@gmail.com>
2023-11-16 15:31:51 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 let cont = 1;
 let fatorial = 1;
 let numeroPedido = prompt("Informe um  número inteiro e positivo:");
-numeroPedido = Number(numeroPedido);
-console.log(typeof numeroPedido);
+numeroPedido = Number(numeroPedido); //convertendo numeroPedido para tipo number
 for(cont = 1; cont <= numeroPedido; cont++ ){
     fatorial *= cont; 
 }
-alert("O fatorial desse número é: " +fatorial);
+alert("O fatorial desse número é: " + fatorial);
