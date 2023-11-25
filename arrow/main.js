b1291b66fb421930208bd9bb99b239f2048715df
Gustavo Mendes <gustavormendess@gmail.com>
2023-11-25 11:13:21 -0400

    Arrow Function JS
    Praticando Arrow Function

---
+++ arrow/main.js
@@ -0,0 +1,13 @@
+//Arrow Function
+const somar =(a,b) => console.log(a + b); 
+somar(20,10);
+
+const multiplicar = (number1 = 0,number2 = 0) => {
+    return number1 * number2;
+}
+console.log(multiplicar(10,10));
+
+const exibir = () => {
+    console.log('Deus é muito bom!');
+}
+exibir();
