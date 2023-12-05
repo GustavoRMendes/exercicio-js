2021d31ff8ebef1527d1931ad70e67ac61afeab5
Gustavo Mendes <gustavormendess@gmail.com>
2023-12-04 22:48:00 -0400

    Exercício 2 JS

---
+++ ex2/main.js
@@ -0,0 +1,5 @@
+const divisivel = (numero1,numero2) => numero1 % numero2 === 0;
+console.log(divisivel(10,3));
+console.log(divisivel(10,5));
+console.log(divisivel(10,2));
+console.log(divisivel(10,7));
