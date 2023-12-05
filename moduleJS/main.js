0bfe6f95a98a274ae4918c7096c572b67e3af89a
Gustavo Mendes <gustavormendess@gmail.com>
2023-12-05 09:36:39 -0400

    Module JS

---
+++ moduleJS/main.js
@@ -0,0 +1,20 @@
+export function mensagem(){
+   alert('top');
+}
+
+export let nome = 'Gustavo'
+
+export function calcular(number,number2){
+    return number % number2 === 0; 
+}
+
+export const arr = ['Gustavo','Felipe','Gaby'];
+
+export const obj = {
+    nome:'Maria',
+    idade:19,
+    time:'Flamengo',
+    programadora:true
+}
+
+export default 50
