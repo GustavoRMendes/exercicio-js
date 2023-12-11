14dec7de50f87b5480211e0c6fa550d5777c6451
Gustavo Mendes <gustavormendess@gmail.com>
2023-12-11 13:23:49 -0400

    Try e Catch JS

---
+++ TryCatch/try.js
@@ -0,0 +1,11 @@
+const resultado = () => {
+  try {
+    codigo;
+  } catch (error) {
+    console.error(` Aconteceu um erro no código. O erro é esse: ${error}`); 
+  } finally {
+    console.info("Essa mensagem sempre será mostrado");
+  }
+};
+
+resultado();
