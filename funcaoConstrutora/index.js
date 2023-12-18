7b8ba6d53420caf4b0fbbd45e4ec4c2f8d7146ae
Gustavo Mendes <gustavormendess@gmail.com>
2023-12-18 11:13:37 -0400

    Funções Construtora JS

---
+++ funcaoConstrutora/index.js
@@ -0,0 +1,12 @@
+// Funções Construtoras
+
+function Carro (nome,marca) {
+    this.nome = nome
+    this.marca = marca
+}
+
+const carro1 = new Carro("S-10","Chevrolet")
+console.log(carro1);
+const carro2 = new Carro("Corolla","Toyota")
+console.log(carro2);
+
