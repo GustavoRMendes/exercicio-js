d839c02b324a4029f3aaff437905dbb4df6b1da9
Gustavo Mendes <gustavormendess@gmail.com>
2023-12-21 19:11:12 -0400

    Destructuring JS

---
+++ destructuring/main.js
@@ -0,0 +1,12 @@
+// Destructuring JS
+
+const array = [1, 2, 3, 4, 5];
+const [z, y] = array;
+console.log(z); //1
+console.log(y); //2
+
+const nomes = ["Gustavo", "Felipe"];
+const [canhoto, destro] = nomes;
+
+console.log(canhoto); //Gustavo
+console.log(destro);  //Felipe
