e6d049eb0c6a30a1d9518758792223d9ad3d0366
Gustavo Mendes <gustavormendess@gmail.com>
2024-01-07 18:05:31 -0400

    Add files via upload

---
+++ objetosGlobais/index.js
@@ -0,0 +1,18 @@
+// Math, Date, Number, Array, Object , String
+
+console.log(Math.PI)
+console.log(Math.sqrt(64))
+console.log(Math.floor(1.66))
+console.log(Math.pow(2,2))
+
+console.log(Date())
+
+console.log(Number("23"))
+
+let lista = Array(1,2,4)
+console.log(lista)
+
+let person = Object({nome:"Gustavo"})
+console.log(person)
+
+console.log(String(23))
