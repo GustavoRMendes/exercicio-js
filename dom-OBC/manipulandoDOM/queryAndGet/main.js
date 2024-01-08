81ace6b877290767a16a145bbe3f37ff22ad8a39
Gustavo Mendes <gustavormendess@gmail.com>
2024-01-08 19:51:05 -0400

    Add files via upload

---
+++ dom-OBC/manipulandoDOM/queryAndGet/main.js
@@ -0,0 +1,9 @@
+const ul = document.querySelector("#contact-list");
+const li = document.querySelector("li");
+const contact1 = document.querySelector("#contact1");
+const contact2 = document.querySelector("#contact2");
+const contact3 = document.querySelector("#contact3");
+const contactInput = document.querySelectorAll(".contact-input");
+function show() {
+  console.log(document.querySelector('#contact-list'))
+}
