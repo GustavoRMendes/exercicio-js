dab99dc12c736bf21c7afbe2ce0ba8c3987667ce
Gustavo Mendes <gustavormendess@gmail.com>
2024-01-08 19:51:33 -0400

    Add files via upload

---
+++ dom-OBC/manipulandoDOM/events/main.js
@@ -0,0 +1,15 @@
+function onMouseEnter() {
+  console.log("Mouse Enter");
+}
+function onFocus() {
+  console.log("Foco");
+}
+function ofFocus() {
+  console.log("Desfoco");
+}
+function onClick() {
+  alert("Clicou");
+}
+function onChange() {
+  console.log("Mudou o valor");
+}
