d178f93a611c0f419c6fffc25663ac97f0ee4200
Gustavo Mendes <gustavormendess@gmail.com>
2024-01-10 20:56:46 -0400

    Add files via upload

---
+++ logicaJS/1/9/main.js
@@ -0,0 +1,13 @@
+function maiorNumero(value1,value2){
+    if(value1 > value2){
+        
+        return value1
+    }else{
+        return value2
+    }
+}
+
+let value1 = Math.PI * 50
+let value2 = Math.PI * 100
+
+console.log(maiorNumero(value1,value2)) //value 2
