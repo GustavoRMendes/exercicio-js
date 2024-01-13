f168ac82363e6f0df4bbb5437cc27a4aaa5f16b8
Gustavo Mendes <gustavormendess@gmail.com>
2024-01-13 19:48:08 -0400

    Add files via upload

---
+++ projetoModerno/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path')
+
+module.exports = {
+
+  devServer: {
+    static:{
+      directory:path.resolve(__dirname,'dist')
+    },
+    compress:true,
+    port:9000
+  },
+
+  entry: {
+    index: "./src/index.js",
+  },
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.js$/,
+        use: ["babel-loader"],
+      },
+    ],
+  },
+  output: {
+    filename:'[name].min.js'
+  }
+};
